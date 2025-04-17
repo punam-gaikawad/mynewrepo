0420f286c8313291011b667befd31d7e9d3c0bf0
punam-gaikawad <gaikawadpunam24@gmail.com>
2025-04-17 13:37:55 +0530

    secind commit

--- Punam.java
+++ Punam.java
@@ -1,7 +1,7 @@
-public class Punam
-{
-    public static void main(String[] args)
-    {
-        System.out.println(“ World”);
+// Your First Program
+
+class Punam {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
